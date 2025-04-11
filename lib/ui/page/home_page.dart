809e0e16e015ca91262f223be442cc31de5b54c6
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2025-04-11 14:16:45 -0300

    ...

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -10,6 +10,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    CircleAvatar(radius: 30, backgroundColor: Colors.blue),
+                    Text('40%'),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
