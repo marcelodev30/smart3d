34c1b75034d68aeba38409b119b46de239266c2f
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2025-04-11 14:06:11 -0300

    ...

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:smart3d/ui/page/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
